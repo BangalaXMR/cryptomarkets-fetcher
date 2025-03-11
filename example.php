3dde612560ef713481c7e6e73bfa2a7ca6bfc3ac
BangalaXMR <bangalaxmr@proton.me>
2025-03-11 12:24:41 +0000

    Update example.php

--- example.php
+++ example.php
@@ -1,29 +1,24 @@
 <?php
 include "markets.php";
 
-$bisqXMRBTC = fetch_bisq_data("XMR_BTC");
-echo "Price of XMR on bisq: " . $bisqXMRBTC['price'] . " BTC\n";
+// Example usage
+$results = [
+    'bisq' => process_bisq_data('xmr_btc'),
+    'tradeogre' => process_tradeogre_data('XMR-BTC'),
+    'haveno' => process_haveno_data('USD'),
+    'nonkyc' => process_nonkyc_data('XMR_USDT'),
+    'kraken' => process_kraken_data('xmrusd'),
+    'mexc' => process_mexc_data('XMRUSDT'),
+    'coinex' => process_coinex_data('XMRUSDT'),
+    'kucoin' => process_kucoin_data('XMR-USDT'),
+    'bitmart' => process_bitmart_data('XMR_USDT')
+];
 
-$havenoBTCXMR = fetch_haveno_data("BTC");
-echo "Price of 1 BTC in XMR on haveno: " . $havenoBTCXMR['price'] . " BTC\n";
-
-$tradeogreXMRBTC = fetch_tradeogre_data("XMR-BTC");
-echo "Price of XMR on tradeogre: " . $tradeogreXMRBTC['price'] . " BTC\n";
-
-$nonkycXMRBTC = fetch_nonkyc_data("XMR_BTC");
-echo "Price of XMR on nonkyc: " . $nonkycXMRBTC['price'] . " BTC\n";
-
-$krakenXMRBTC = fetch_kraken_data("XMRXBT");
-echo "Price of XMR on kraken: " . $krakenXMRBTC['price'] . " BTC\n";
-
-$mexcXMRUSDT = fetch_mexc_data("XMRUSDT");
-echo "Price of XMR on mexc: " . $mexcXMRUSDT['price'] . " USDT\n";
-
-$coinexXMRUSDT = fetch_coinex_data("XMRUSDT");
-echo "Price of XMR on coinex: " . $coinexXMRUSDT['price'] . " USDT\n";
-
-$kucoinXMRUSDT = fetch_kucoin_data("XMR-USDT");
-echo "Price of XMR on kucoin: " . $kucoinXMRUSDT['price'] . " USDT\n";
-
-$bitmartXMRBTC = fetch_bitmart_data("XMR_BTC");
-echo "Price of XMR on bitmart: " . $bitmartXMRBTC['price'] . " BTC\n";
+// Print the results
+foreach ($results as $key => $result) {
+    if ($result) {
+        echo "Data from $key for " . $result['ticker'] . ":\n";
+        echo "Price: " . $result['price'] . "\n";
+        echo "Volume: " . $result['volume'] . "\n";
+    }
+}
